f78cdc133e5e6466abbe2364902528c104cbe49d
Sean Downs <spdowns@protonmail.com>
2020-10-08 08:54:57 -0700

    add css and img to splash page. adjust certain spacing and add img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
                 font-family: 'Nunito';
             }
         </style>
+        <link href="/css/main.css" rel="stylesheet">
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
@@ -37,10 +38,7 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <h1> {{ App::environment() }} </h1>
-                </div>
-
+                <h1> Clashic Journal </h1>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
